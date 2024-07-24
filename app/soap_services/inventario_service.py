2ffcdf58b29bfeb5d80908dbce7270a625caf421
MMartinHan <mmhan@espe.edu.ec>
2024-07-23 20:32:21 -0500

    SOAP

--- app/soap_services/inventario_service.py
+++ app/soap_services/inventario_service.py
@@ -1,28 +1,31 @@
+# soap_service.py
+from flask import Flask
 from spyne import Application, rpc, ServiceBase, Integer, Unicode
 from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
+from extensions import db
+from models import product
 
 class InventarioService(ServiceBase):
     @rpc(Integer, _returns=Unicode)
     def consultar_inventario(ctx, product_id):
-        inventario = {
-            1: {'nombre': 'Producto 1', 'cantidad': 50},
-            2: {'nombre': 'Producto 2', 'cantidad': 20}
-        }
-        producto = inventario.get(product_id, None)
-        return str(producto) if producto else 'Producto no encontrado'
+        with ctx.udc.app.app_context():
+            producto = product.Product.query.filter_by(product_id=product_id).first()
+            if producto:
+                return f'Nombre: {producto.product_name}, Cantidad: {producto.product_stock}'
+            else:
+                return 'Producto no encontrado'
 
     @rpc(Integer, Integer, _returns=Unicode)
     def actualizar_inventario(ctx, product_id, cantidad):
-        inventario = {
-            1: {'nombre': 'Producto 1', 'cantidad': 50},
-            2: {'nombre': 'Producto 2', 'cantidad': 20}
-        }
-        if product_id in inventario:
-            inventario[product_id]['cantidad'] = cantidad
-            return 'Actualización exitosa'
-        else:
-            return 'Producto no encontrado'
+        with ctx.udc.app.app_context():
+            producto = product.Product.query.filter_by(product_id=product_id).first()
+            if producto:
+                producto.product_stock = cantidad
+                db.session.commit()
+                return 'Actualización exitosa'
+            else:
+                return 'Producto no encontrado'
 
 def init_app(app):
     soap_app = Application([InventarioService], 'spyne.examples.flask',
@@ -37,6 +40,7 @@ def init_app(app):
 
         def __call__(self, environ, start_response):
             if environ['PATH_INFO'].startswith('/api/soap'):
+                environ['spyne.udc'] = {'app': self.app}
                 return self.wsgi_soap_app(environ, start_response)
             return self.app(environ, start_response)
 
