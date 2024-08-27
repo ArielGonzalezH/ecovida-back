0864ea53de56499faf9b3b41576f9f2f36437cfb
MMartinHan <mmhan@espe.edu.ec>
2024-08-27 00:06:50 -0500

    sales funtions

--- app/models/sale_item.py
+++ app/models/sale_item.py
@@ -1,16 +1,16 @@
 from extensions import db
 
 class Sale_Item(db.Model):
-    __tablename__ = 'SALE_ITEM'
+    __tablename__ = 'SALE_ITEMS'
     item_id = db.Column(db.Integer, primary_key=True)
     sale_header_id = db.Column(db.Integer, nullable=False)
     product_id = db.Column(db.Integer, nullable=False)
-    sale_quantity = db.Column(db.Integer, nullable=False)
+    item_quantity = db.Column(db.Integer, nullable=False)
 
     def as_dict(self):
         return {
             "item_id": self.item_id,
             "sale_header_id": self.sale_header_id,
             "product_id": self.product_id,
-            "sale_quantity": self.sale_quantity
+            "item_quantity": self.item_quantity
         }
